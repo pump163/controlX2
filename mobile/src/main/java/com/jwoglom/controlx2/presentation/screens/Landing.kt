ad0632da6ae6bbb596bb33ef6575aa264a3d9176
pumppan <pump@163.com>
2026-09-21 00:21:28 +0800

    修复导入错误

--- mobile/src/main/java/com/jwoglom/controlx2/presentation/screens/Landing.kt
+++ mobile/src/main/java/com/jwoglom/controlx2/presentation/screens/Landing.kt
@@ -53,8 +53,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveableCoroutineScope
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
